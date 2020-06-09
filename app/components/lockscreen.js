cb31ed1050f8988df794d6244ef4cce4bb372109
Matthias Metz <matthias.metz@htl.rennweg.at>
2020-06-09 15:26:58 +0200

    Lockscreen mit Tastatureingabe + Buttons bei Vanessa, Mathe und Mathelehrer fertig

--- app/components/lockscreen.js
+++ app/components/lockscreen.js
@@ -6,20 +6,29 @@ app.component("lockscreen", {
     bindings:{}
 });
 
-app.controller("LockscreenController", function ($log, $state, AccessKontakt) {
+app.controller("LockscreenController", function ($log, $state, AccessKontakt, $timeout) {
     $log.debug("LockscreenController()");
 
     this.password = new Array(6);
     this.password = [2,3,0,3,0,1];
     this.aktpasswort = [];
 
-    this.keypress = () =>{
-        console.log("KEYPRESSED");
+    document.onkeypress = (event) =>{
+        console.log(event);
+        this.zahleingegeben(Number(event.key));
+
+        if(event.key === "Delete"){
+            console.log("Delete KEy");
+        }
+
+        $timeout();
     }
 
     this.zahleingegeben = (pwnumber) => {
         AccessKontakt.playAudioclick();
         this.aktpasswort[this.aktpasswort.length] = pwnumber;
+        console.log("akt passwort" , this.aktpasswort);
+        console.log("passwort" , this.password);
         if(this.aktpasswort.length === this.password.length){
             if(angular.equals(this.aktpasswort, this.password)){
                 AccessKontakt.allowbutton();
